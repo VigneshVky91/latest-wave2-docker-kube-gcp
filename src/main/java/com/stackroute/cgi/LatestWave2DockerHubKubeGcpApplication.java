d77f03d90e521b7ecf172b8d20acce58fee9cbd8
VigneshVky91 <vigneshvky91@gmail.com>
2022-04-14 10:02:08 +0530

    First commit

---
+++ src/main/java/com/stackroute/cgi/LatestWave2DockerHubKubeGcpApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.cgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestWave2DockerHubKubeGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatestWave2DockerHubKubeGcpApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/stackroute/cgi/Wave2Controller.java
@@ -0,0 +1,13 @@
+package com.stackroute.cgi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Wave2Controller {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to IBM NHT Wave 2 batch";
+	}
+}
